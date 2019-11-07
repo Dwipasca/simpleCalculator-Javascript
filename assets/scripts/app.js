0406b4427d9700df47557a25e40eacb049fbee2c
Suck To Be Me <dpnursanti@gmail.com>
2019-11-07 17:50:34 +0800

    function add, minus, multiple and divide done

---
+++ assets/scripts/app.js
@@ -0,0 +1,50 @@
+console.log("app is active0");
+const defaultResult = 0;
+let currentResult = defaultResult;
+// get value from userinput
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+//show result calc
+function createAndwriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+//process add number
+function add() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enterNumber;
+  createAndwriteOutput("+", initialResult, enterNumber);
+}
+
+//process minus number
+function minus() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enterNumber;
+  createAndwriteOutput("-", initialResult, enterNumber);
+}
+
+// process multiple number
+function multiple() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enterNumber;
+  createAndwriteOutput("x", initialResult, enterNumber);
+}
+
+//process divide number
+function divide() {
+  const enterNumber = getUserInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enterNumber;
+  createAndwriteOutput(":", initialResult, enterNumber);
+}
+
+//when the button click will be call the function
+addBtn.addEventListener("click", add);
+minusBtn.addEventListener("click", minus);
+multipleBtn.addEventListener("click", multiple);
+divideBtn.addEventListener("click", divide);
